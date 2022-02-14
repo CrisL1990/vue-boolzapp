e500d9a5f35c335f6e00104b28e3f8e71e9b8132
CrisL1990 <lucchetta.cris@gmail.com>
2022-02-14 11:39:48 +0100

    Partenza esercizio

---
+++ main.js
@@ -0,0 +1,110 @@
+const root = new Vue({
+    
+    el: "#root",
+
+    data:{
+
+        contacts: [
+
+            {
+                name: "Michele",
+                avatar: '_1',
+                visible: true,
+                messages:[
+
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'Hai portato a spasso il cane?',
+                        statue: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'Hai portato a spasso il cane?',
+                        statue: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'Hai portato a spasso il cane?',
+                        statue: 'received'
+                    }
+                ],
+                
+            },
+
+            {
+                name: "Fabio",
+                avatar: '_2',
+                visible: true,
+                messages:[
+
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'Hai portato a spasso il cane?',
+                        statue: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'Hai portato a spasso il cane?',
+                        statue: 'received'
+                    },
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'Hai portato a spasso il cane?',
+                        statue: 'sent'
+                    }
+                ],
+                
+            },
+
+            {
+                name: "Samuele",
+                avatar: '_3',
+                visible: true,
+                messages:[
+
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'Hai portato a spasso il cane?',
+                        statue: 'received'
+                    },
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'Hai portato a spasso il cane?',
+                        statue: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'Hai portato a spasso il cane?',
+                        statue: 'received'
+                    }
+                ],
+                
+            },
+
+            {
+                name: "Luisa",
+                avatar: '_4',
+                visible: true,
+                messages:[
+
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'Hai portato a spasso il cane?',
+                        statue: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'Hai portato a spasso il cane?',
+                        statue: 'received'
+                    },
+
+                ],
+                
+            }
+        ]
+    },
+
+    methods:{
+        
+    }
+})
